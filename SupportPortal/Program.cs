a566891d3fa6081c6b6f6bb21bcff471ae35ce1c
njabulo <njebzeliny@gmail.com>
2024-07-27 20:17:07 +0200

    20

--- SupportPortal/Program.cs
+++ SupportPortal/Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using NLog;
+using SupportPortal.Extensions;
+using SupportPortal.JwtFeatures;
+
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJWT(builder.Configuration);
+builder.Services.AddScoped<JwtHandler>();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureSwagger();
+builder.Services.AddAuthentication();
+
+builder.Services.Configure<DataProtectionTokenProviderOptions>(opt =>
+    opt.TokenLifespan = TimeSpan.FromHours(2));
 
 builder.Services.AddControllers();
 
@@ -10,7 +31,18 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Inventory System API v1");
+    s.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+});
 
 app.MapControllers();
 
