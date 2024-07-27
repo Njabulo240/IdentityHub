155a540681f16874e7dc9b86479768c84c3366f4
njabulo <njebzeliny@gmail.com>
2024-07-27 23:01:11 +0200

    35

--- SupportPortalUI/src/app/home/home.module.ts
+++ SupportPortalUI/src/app/home/home.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroComponent } from './hero/hero.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ HomeComponent,HeroComponent]
+  declarations: [ 
+    HomeComponent,
+    HeroComponent,
+    SponsorsComponent,
+    BlogComponent
+  ]
 })
 export class HomeModule { }
