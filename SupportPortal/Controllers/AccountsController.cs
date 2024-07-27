ff8bb1b96d5cc736fe18f9f986d3ff1d4d1b111c
njabulo <njebzeliny@gmail.com>
2024-07-27 20:24:03 +0200

    23

---
+++ SupportPortal/Controllers/AccountsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using EmailService;
+using Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTO.Account;
+using SupportPortal.JwtFeatures;
+
+namespace SupportPortal.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        private readonly JwtHandler _jwtHandler;
+        private readonly IEmailSender _emailSender;
+
+        public AccountsController(UserManager<User> userManager, IMapper mapper, JwtHandler jwtHandler,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _jwtHandler = jwtHandler;
+            _emailSender = emailSender;
+        }
+
+        [HttpPost("Registration")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+
+                return BadRequest(new RegistrationResponseDto { Errors = errors });
+            }
+
+            await _userManager.AddToRoleAsync(user, "User");
+
+            return StatusCode(201);
+        }
+
+    }
+}
