154b7fb4d52a299d5d6349acb50be03cb9a9313b
njabulo <njebzeliny@gmail.com>
2024-07-27 19:07:08 +0200

    12

---
+++ Shared/DTO/Account/UserForRegistrationDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTO.Account
+{
+    public class UserForRegistrationDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "UserName is required.")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
