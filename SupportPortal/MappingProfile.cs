f40b080509f868ba6b936a0150e1390ba5aa4bb4
njabulo <njebzeliny@gmail.com>
2024-07-27 20:28:12 +0200

    28

---
+++ SupportPortal/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Entities.Identity;
+using Shared.DTO.Account;
+
+namespace SupportPortal
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserForRegistrationDto, User>();
+            CreateMap<User, UserDto>();
+            CreateMap<UserRole, UserRoleDto>();
+            CreateMap<UserForUpdateDto, User>();
+            CreateMap<UserRoleForCreationDto, UserRole>();
+            CreateMap<UserRoleForUpdateDto, UserRole>();
+        }
+    }
+}
