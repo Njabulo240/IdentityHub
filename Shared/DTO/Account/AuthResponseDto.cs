d229c0369d2d23750d0bde3613e6e618349cc8fa
njabulo <njebzeliny@gmail.com>
2024-07-27 19:02:34 +0200

    8

---
+++ Shared/DTO/Account/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DTO.Account
+{
+    public class AuthResponseDto
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? Token { get; set; }
+    }
+}
