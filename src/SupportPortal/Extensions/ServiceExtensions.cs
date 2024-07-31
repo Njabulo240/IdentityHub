9a644794b16af9af21d69ccd83a6ae0634a768d0
njabulo <njebzeliny@gmail.com>
2024-07-31 15:59:11 +0200

    48

--- src/SupportPortal/Extensions/ServiceExtensions.cs
+++ src/SupportPortal/Extensions/ServiceExtensions.cs
@@ -1,6 +1,13 @@
 ﻿using Contracts;
+using Entities.Identity;
 using LoggerService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Repository;
+using Repository.Data;
+using System.Text;
 
 namespace SupportPortal.Extensions
 {
@@ -12,15 +19,73 @@ namespace SupportPortal.Extensions
             {
 
             });
-
-
+        public static void ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JWT");
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                 .WithOrigins(jwtSettings["ClientUrl"])
+                  .AllowCredentials()
+                   .AllowAnyHeader()
+                .AllowAnyHeader());
+            });
+        }
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
            services.AddScoped<IRepositoryManager, RepositoryManager>();
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
     services.AddSingleton<ILoggerManager, LoggerManager>();
 
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+    services.AddDbContext<Context>(opts =>
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<User>(options =>
+            {
+                // password configuration
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                // for email confirmation
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+              .AddRoles<IdentityRole>()
+              .AddRoleManager<RoleManager<IdentityRole>>()
+              .AddEntityFrameworkStores<Context>()
+              .AddSignInManager<SignInManager<User>>()
+              .AddUserManager<UserManager<User>>()
+              .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JWT");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // Validate the token based on the key we have provided inside appsettings.json JWT:Key
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Key")),
+                        // The issuer (who is issuing the JWT)
+                        ValidIssuer = jwtSettings["Issuer"],
+                        // Validate the issuer (whoever is issuing the JWT)
+                        ValidateIssuer = true,
+                        // Don't validate audience (depending on your needs)
+                        ValidateAudience = false
+                    };
+                });
+        }
 
     }
 }
--- src/SupportPortal/Program.cs
+++ src/SupportPortal/Program.cs
@@ -1,75 +1,25 @@
-
-using Entities.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Repository.Data;
 using Repository.Services;
 using Shared;
 using SupportPortal;
+using SupportPortal.Extensions;
 using System.Security.Claims;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<Context>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-// be able to inject JWTService class inside our Controllers
+builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddScoped<JWTService>();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<ContextSeedService>();
-
-// defining our IdentityCore Service
-builder.Services.AddIdentityCore<User>(options =>
-{
-    // password configuration
-    options.Password.RequiredLength = 6;
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-
-    // for email confirmation
-    options.SignIn.RequireConfirmedEmail = true;
-})
-    .AddRoles<IdentityRole>() // be able to add roles
-    .AddRoleManager<RoleManager<IdentityRole>>() // be able to make use of RoleManager
-    .AddEntityFrameworkStores<Context>() // providing our context
-    .AddSignInManager<SignInManager<User>>() // make use of Signin manager
-    .AddUserManager<UserManager<User>>() // make use of UserManager to create users
-    .AddDefaultTokenProviders(); // be able to create tokens for email confirmation
-
-// be able to authenticate users using JWT
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            // validate the token based on the key we have provided inside appsettings.development.json JWT:Key
-            ValidateIssuerSigningKey = true,
-            // the issuer singning key based on JWT:Key
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
-            // the issuer which in here is the api project url we are using
-            ValidIssuer = builder.Configuration["JWT:Issuer"],
-            // validate the issuer (who ever is issuing the JWT)
-            ValidateIssuer = true,
-            // don't validate audience (angular side)
-            ValidateAudience = false
-        };
-    });
-
+builder.Services.ConfigureCors(builder.Configuration);
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJWT(builder.Configuration);
 builder.Services.AddCors();
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
@@ -109,11 +59,7 @@ builder.Services.AddAuthorization(opt =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseCors(opt =>
-{
-    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["JWT:ClientUrl"]);
-});
+app.UseCors("CorsPolicy");
 
 if (app.Environment.IsDevelopment())
 {
@@ -122,9 +68,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-// adding UseAuthentication into our pipeline and this should come before UseAuthorization
-// Authentication verifies the identity of a user or service, and authorization determines their access rights.
 app.UseAuthentication();
 app.UseAuthorization();
 
